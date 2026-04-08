d312e4b421826c5dd8ba67fc8e79fdc76a6f6c0e
Vitor Trindade Borher <vitortrindadeborher@gmail.com>
2026-04-08 18:58:52 -0300

    Add seed data for Igrejas and membros_pg tables

--- Database/seed.sql
+++ Database/seed.sql
@@ -1,1 +1,11 @@
+insert into Igrejas (Nome, Localiza)
+values ('Mevam', 'Deodoro'),
+('Assembléia', 'Miguel couto'),
+('Batista', 'Sjm'),
+('Presbiteriana', 'Campo Grande');
 
+insert into membros_pg (Nome, Data_de_nascimento, Nome_dos_responsaveis, igreja_id)
+values ('Vitor', '2008-11-11', 'Sérgio e léa', 1),
+('Cristian', '2008-06-23', 'Luciano e Elisabete ', 2),
+('Léa', '1975-03-09', 'Maria e Ademir', 3),
+('Sérgio', '1976-05-26', 'Mariza e José', 4);
