4aaa44f72fad5c7bd978754216dc1186f22837cd
Vitor Trindade Borher <vitortrindadeborher@gmail.com>
2026-04-08 19:02:10 -0300

    Create tables for membros_pg and Igrejas

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,1 +1,15 @@
+create table membros_pg (
+id serial primary key,
+Nome varchar Not null,
+Data_de_nascimento date not null,
+Nome_dos_responsaveis varchar not null,
+igreja_id int not null,
+foreign key (igreja_id) references Igrejas(Id)
+);
+
+create table Igrejas (
+Id serial primary key,
+Nome Varchar,
+Localiza Varchar
+);
 
